500f446fa742c7534a77c8ff57c9bb94dc9c866d
bunny <bunny@techbang.com.tw>
2020-02-06 12:10:52 +0800

    addGemHirb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_023944) do
+ActiveRecord::Schema.define(version: 2020_02_06_031429) do
 
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
