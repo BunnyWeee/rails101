b22acd93326648f94c276ff72b7f135090aaf58c
bunny <bunny@techbang.com.tw>
2020-02-10 18:06:17 +0800

    add faker group seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Generate 10 groups..."
+10.times do |g|
+  Group.create(title:Faker::Games::LeagueOfLegends.champion,
+               description:Faker::Games::LeagueOfLegends.location,
+               user_id: [*1..6].sample)
+end
+puts "done!"
