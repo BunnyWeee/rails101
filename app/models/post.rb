9d1df8c1aa0aa632d50add06a1970e9ac9a92767
bunny <bunny@techbang.com.tw>
2020-02-11 10:32:50 +0800

    correct after PR

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :group, counter_cache: true
   belongs_to :user
   
-  scope :recent,  -> { order(updated_at: :DESC) }
+  scope :recent, -> { order(updated_at: :DESC) }
 end
