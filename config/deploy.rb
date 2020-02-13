c3e5fc409e2c92b311175bd7a728f1e9a54d7ffb
bunny <bunny@techbang.com.tw>
2020-02-13 16:36:54 +0800

    fix secret_key_base

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,12 +9,6 @@ set :branch, "t21555"
 
 set :deploy_to, "home/deploy/rail/rails101"
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.6.3'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
-
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/settings.yml"
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", '.bundle'
