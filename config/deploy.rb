1f771e5b088e6cb17fcfc8ea41c44bb33a626752
bunny <bunny@techbang.com.tw>
2020-02-13 14:17:00 +0800

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,8 @@ set :branch, "t21555"
 
 set :deploy_to, "home/deploy/rail/rails101"
 
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
+
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/settings.yml"
 
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", '.bundle'
